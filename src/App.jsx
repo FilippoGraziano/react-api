7e33206e6dff015601579ccdf599e56a87fe9a39
Filippo Graziano <filippograziano13@gmail.com>
2026-07-25 13:55:33 +0200

    add list of actress to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import './App.css'
+import axios from "axios"
+import { useEffect, useState } from 'react'
 
 const App = () => {
 
+  const [actressList, setActressList] = useState([])
+
+  const fetchActress = () => {
+
+    axios.get(`https://lanciweb.github.io/demo/api/actresses/`)
+      .then(res => setActressList(res.data))
+      
+  }
+
+  useEffect(fetchActress, [])
+
+
+
   return (
     <>
-      
+      <ul id="actress-list">
+
+        <li>
+          <h2>Actress</h2>
+        </li>
+
+        {actressList.map(el =>
+
+          <li key={el.id}>{el.name}</li>
+
+        )}
+
+      </ul>
     </>
   )
 }
